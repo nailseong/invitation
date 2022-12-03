3ad34cbe01d48699140f5ed877fcfe9589558302
nailseong <nnis9857@gmail.com>
2022-12-04 01:27:33 +0900

    chore: ui 모듈

---
+++ invitation-ui/src/test/java/com/nailseong/invitation/InvitationApplicationTest.java
@@ -0,0 +1,12 @@
+package com.nailseong.invitation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InvitationApplicationTest {
+
+    @Test
+    void contextLoad() {
+    }
+}
