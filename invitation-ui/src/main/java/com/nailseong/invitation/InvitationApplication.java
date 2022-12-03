8de6f2eff3e7483f6fdcd74598a75bf7c54ac839
Na Ilseong <ilseongdev@gmail.com>
2022-12-04 01:57:20 +0900

    chore: set up multi module (#2)
    * chore: ui 모듈
    
    * chore: domain 모듈
    
    * chore: infrastructure 모듈
    
    * test: application 로딩

--- invitation-ui/src/main/java/com/nailseong/invitation/InvitationApplication.java
+++ invitation-ui/src/main/java/com/nailseong/invitation/InvitationApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InvitationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InvitationApplication.class, args);
-	}
-
+    public static void main(final String[] args) {
+        SpringApplication.run(InvitationApplication.class, args);
+    }
 }
--- invitation-ui/src/test/java/com/nailseong/invitation/InvitationApplicationTest.java
+++ invitation-ui/src/test/java/com/nailseong/invitation/InvitationApplicationTest.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InvitationApplicationTests {
+class InvitationApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoad() {
+    }
 }
