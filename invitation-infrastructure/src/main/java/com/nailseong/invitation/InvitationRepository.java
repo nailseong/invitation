e3db2011b8bd49432d9216737f091844dd49aa99
nailseong <nnis9857@gmail.com>
2022-12-04 15:57:03 +0900

    feat: InvitationRepository

---
+++ invitation-infrastructure/src/main/java/com/nailseong/invitation/InvitationRepository.java
@@ -0,0 +1,6 @@
+package com.nailseong.invitation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvitationRepository extends JpaRepository<InvitationEntity, Long> {
+}
