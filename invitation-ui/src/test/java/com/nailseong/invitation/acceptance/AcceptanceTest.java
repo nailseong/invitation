e4d0527c1b05e0613030c4a4d87f19d2a30fe9ce
nailseong <nnis9857@gmail.com>
2022-12-05 00:04:08 +0900

    refactor(AcceptanceTest): DatabaseCleaner 적용

--- invitation-ui/src/test/java/com/nailseong/invitation/acceptance/AcceptanceTest.java
+++ invitation-ui/src/test/java/com/nailseong/invitation/acceptance/AcceptanceTest.java
@@ -1,7 +1,9 @@
 package com.nailseong.invitation.acceptance;
 
+import com.nailseong.invitation.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -11,8 +13,12 @@ abstract class AcceptanceTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+        databaseCleaner.clean();
     }
 }
