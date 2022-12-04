0579c480de62d9b5cd6ddb84260567ea2ad6e608
nailseong <nnis9857@gmail.com>
2022-12-04 15:44:44 +0900

    refactor: MemberEntity 테이블 이름

--- invitation-infrastructure/src/main/java/com/nailseong/member/MemberEntity.java
+++ invitation-infrastructure/src/main/java/com/nailseong/member/MemberEntity.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "member")
 public class MemberEntity extends BaseEntity {
 
     @Column(nullable = false)
