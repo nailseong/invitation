203afea7ef98585fd1a08d5b32728246c0b851d2
nailseong <nnis9857@gmail.com>
2022-12-04 16:22:25 +0900

    chore: AcceptanceTest

---
+++ invitation-ui/src/test/java/com/nailseong/acceptance/AcceptanceTest.java
@@ -0,0 +1,23 @@
+package com.nailseong.acceptance;
+
+import com.nailseong.TestConfiguration;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import({
+        TestConfiguration.class,
+})
+abstract class AcceptanceTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+}
