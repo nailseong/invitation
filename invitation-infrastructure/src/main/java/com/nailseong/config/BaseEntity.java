dc76c7031ae698d3d57e17edf8fad8c717b533d5
nailseong <nnis9857@gmail.com>
2022-12-04 17:33:21 +0900

    refactor(BaseEntity): eq&hc

--- invitation-infrastructure/src/main/java/com/nailseong/config/BaseEntity.java
+++ invitation-infrastructure/src/main/java/com/nailseong/config/BaseEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -38,5 +39,22 @@ public abstract class BaseEntity {
 
     public LocalDateTime getUpdatedAt() {
         return updatedAt;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
