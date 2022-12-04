04f59e9026482e01a8182ceddb5d9b9348fc3907
nailseong <nnis9857@gmail.com>
2022-12-04 16:56:19 +0900

    chore: `@EnableJpaRepositories`

--- invitation-infrastructure/src/main/java/com/nailseong/config/JpaConfig.java
+++ invitation-infrastructure/src/main/java/com/nailseong/config/JpaConfig.java
@@ -2,8 +2,10 @@ package com.nailseong.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories
 public class JpaConfig {
 }
