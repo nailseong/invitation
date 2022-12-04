671a3d256ec20cb471ebb0e3267d641074f8f561
nailseong <nnis9857@gmail.com>
2022-12-04 23:30:51 +0900

    chore(MemberService): `@Transactional`

--- invitation-domain/src/main/java/com/nailseong/invitation/member/application/MemberService.java
+++ invitation-domain/src/main/java/com/nailseong/invitation/member/application/MemberService.java
@@ -3,8 +3,10 @@ package com.nailseong.invitation.member.application;
 import com.nailseong.invitation.member.MemberEntity;
 import com.nailseong.invitation.member.MemberRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepo;
