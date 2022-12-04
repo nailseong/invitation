2a53d003326ac8aa5ab601780aee9d996ee8f683
nailseong <nnis9857@gmail.com>
2022-12-04 17:33:44 +0900

    feat(MemberRepository): findByUsername

--- invitation-infrastructure/src/main/java/com/nailseong/member/MemberRepository.java
+++ invitation-infrastructure/src/main/java/com/nailseong/member/MemberRepository.java
@@ -1,6 +1,9 @@
 package com.nailseong.member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+    Optional<MemberEntity> findByUsername(String username);
 }
