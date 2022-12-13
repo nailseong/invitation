593ccaeb0f1a3f01a9f2cc7c12ae1e96429a570f
nailseong <nnis9857@gmail.com>
2022-12-13 17:35:04 +0900

    feat: login.html

--- invitation-ui/src/main/java/com/nailseong/invitation/view/ViewController.java
+++ invitation-ui/src/main/java/com/nailseong/invitation/view/ViewController.java
@@ -1,5 +1,7 @@
 package com.nailseong.invitation.view;
 
+import com.nailseong.invitation.authentication.presentation.AuthController;
+import com.nailseong.invitation.authentication.presentation.dto.LoginRequest;
 import com.nailseong.invitation.member.MemberController;
 import com.nailseong.invitation.member.dto.SignupRequest;
 import org.springframework.stereotype.Controller;
@@ -11,15 +13,26 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ViewController {
 
+    private final AuthController authController;
     private final MemberController memberController;
 
-    public ViewController(final MemberController memberController) {
+    public ViewController(final AuthController authController,
+                          final MemberController memberController) {
+        this.authController = authController;
         this.memberController = memberController;
     }
 
     @GetMapping("/")
-    public ModelAndView getHome() {
-        return new ModelAndView("index");
+    public ModelAndView getHome(final LoginRequest request) {
+        final ModelAndView view = new ModelAndView("index");
+        view.addObject("request", request);
+        return view;
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@ModelAttribute("request") final LoginRequest request) {
+        authController.login(request);
+        return "redirect:/";
     }
 
     @GetMapping("/signup")
