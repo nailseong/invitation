23542e02077f3ae09b1f5cd3672e671908b0984b
Na Ilseong <ilseongdev@gmail.com>
2022-12-13 18:01:20 +0900

    feat: home and signup view (#26)
    * chore(ui): thymeleaf 의존성
    
    * feat: index.html
    
    * feat: signup.html
    
    * feat: login.html
    
    * feat: LoginView
    
    * feat: 로그인 여부에 따라서 화면을 다르게 구현

---
+++ invitation-ui/src/main/java/com/nailseong/invitation/view/ViewController.java
@@ -0,0 +1,56 @@
+package com.nailseong.invitation.view;
+
+import com.nailseong.invitation.authentication.presentation.AuthController;
+import com.nailseong.invitation.authentication.presentation.dto.LoginRequest;
+import com.nailseong.invitation.authentication.support.LoginSession;
+import com.nailseong.invitation.authentication.support.LoginView;
+import com.nailseong.invitation.member.MemberController;
+import com.nailseong.invitation.member.dto.SignupRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ViewController {
+
+    private final AuthController authController;
+    private final MemberController memberController;
+
+    public ViewController(final AuthController authController,
+                          final MemberController memberController) {
+        this.authController = authController;
+        this.memberController = memberController;
+    }
+
+    @GetMapping("/")
+    public ModelAndView getHome(final LoginRequest request, @LoginView final LoginSession session) {
+        final ModelAndView view = new ModelAndView("index");
+        view.addObject("request", request);
+        view.addObject("isLogin", session != null);
+        if (session != null) {
+            view.addObject("username", session.username());
+        }
+        return view;
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@ModelAttribute("request") final LoginRequest request) {
+        authController.login(request);
+        return "redirect:/";
+    }
+
+    @GetMapping("/signup")
+    public ModelAndView getSignup(final SignupRequest request) {
+        final ModelAndView view = new ModelAndView("signup");
+        view.addObject("request", request);
+        return view;
+    }
+
+    @PostMapping("/signup")
+    public String postSignup(@ModelAttribute("request") final SignupRequest request) {
+        memberController.signup(request);
+        return "redirect:/";
+    }
+}
